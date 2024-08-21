2b84d8b7504ede05195c18d3442b3afbc5f3325e
hhh <2770754682@qq.com>
2024-08-22 00:54:58 +0800

    finish the map of home pages and srceen adapter

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,29 +6,107 @@ Page({
    */
   data: {
     transitionCompleted:false,
-    showTransition:true
+    showTransition:true,
+    current:true,
+    // 关键是这里要写与map组件id对应的！select用.或者#都一样
+    context : wx.createMapContext('map', wx.createSelectorQuery().select('#map')),
+    longitude:119.19245 ,
+    latitude:26.056212,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.startTimer();
+  },
+  startTimer(){
+    let lo = null;
+    let la= null;
+    this.timer = setInterval(()=>{
+      const context = this.data.context
+      const current = this.data.current
+      if(context){
+        context.getCenterLocation({
+          success(res){
+            lo = res.longitude;
+            la =  res.latitude;
+          }
+        })
+        context.moveToLocation()
+        context.getCenterLocation({
+          success(){
+            if(res.longitude != lo || res.latitude != la){
+              this.setData({
+                current:false
+              })
+              console.log('not before pos')
+            }
+          }
+        })
+      }
+    },1000)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-    setTimeout(() => {
-      console.log("页面加载完毕")
-      this.setData({
-        transitionCompleted:true,
-        showTransition:false
-      })
-      //使用这种方式无法实现页面动态更新
-      // this.transitionCompleted = true;   
-    }, 2000); // 调整动画执行的时间
+    // 这里页面初始化的时候调用
+    const context = this.data.context; 
+    //TODO:申请定位，获取定位的地图 接口
+    context.moveToLocation({
+      success(){
+        console.log('s')
+      },
+      complete(){
+        console.log('goback')
+      }
+    })
+    let latitude = this.data.latitude;
+    let longitude = this.data.longitude;
+    context.getCenterLocation({
+      success(res){
+        this.setData({
+          latitude: res.latitude,
+          longitude:res.longitude
+        })
+      },
+      fail(e){
+        console.log(e)
+      }
+    })
+    // 必须要真机调试的时候才能看到标记点
+    //标记当前位置
+    const marker = {
+      id:1,
+      latitude:latitude,
+      longitude:longitude,
+      width:30,
+      height:30
+    }
+    context.addMarkers({
+      markers:[marker],
+      success:function(){
+        console.log('s')
+      },
+      fail(e){
+        console.log(e)
+      },
+      complete(){
+        console.log('marker')
+      }
+    })
+    
+    // setTimeout(() => {
+    //   console.log("页面加载完毕")
+    //   this.setData({
+    //     transitionCompleted:true,
+    //     showTransition:false
+    //   })
+    //   //使用这种方式无法实现页面动态更新
+    //   // this.transitionCompleted = true;   
+    // }, 2000); // 调整动画执行的时间
   },
 
   /**
@@ -75,19 +153,27 @@ Page({
   goIndoor(){
     console.log('indoor')
     wx.navigateTo({
-      url: '../indoor-guidance',
+      url: '../indoor-guidance/indoor-guidance',
       fail(e){
         console.log(e)
       }
     })
   },
-  goBack(){
-    console.log('back')
-    wx.navigateTo({
-      url: '../indoor-guidance',
-      fail(e){
-        console.log(e)
+  goBack(){ 
+    const current = this.data.current;
+    this.setData({
+      current: !current
+    })
+    const context = this.data.context;
+    //回到原始定位点  
+    context.moveToLocation({
+      success(){
+        console.log('s')
+      },
+      complete(){
+        console.log('goback')
       }
     })
+    //TODO:监听页面位置移动
   }
 })
